0f1ad4c1a940d75e807ecf09c4675402be9bf791
MariaVanatoa <maria.h.vanatoa@gmail.com>
2017-11-27 14:16:10 +0200

    27.11

---
+++ Assets/Scripts/GameItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameItem 
+{
+	private string itemName;
+
+	public GameItem (string itemName)
+		{
+			this.itemName = itemName;
+		}
+
+	public string GameItemName() {
+			return this.itemName;
+
+		}
+	}
